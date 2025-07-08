ea3a9a47ccba2ffd1a6c0e23a6f26beb30eac360
saral98 <sarl98@mi.fu-berlin.de>
2025-07-08 17:54:17 +0200

    make package buildable on bioc-submission

--- tests/testthat/test_queryEngine.R
+++ tests/testthat/test_queryEngine.R
@@ -1,12 +1,13 @@
 
 
 test_that("fetch_data works as intented", {
-    
+    eh <- ExperimentHub::ExperimentHub() 
+    centreprecompdb <- eh[["EH9540"]]
     expected <- data.frame(
         pair = c("EH38E3440167_ENSG00000000419"),
         cor_CRUP = c(-0.006860231)
     )
-    fetched <- fetch_data(CENTREprecompDb,
+    fetched <- fetch_data(centreprecompdb,
                           table = "crup_cor",
                           columns = c("pair", "cor_CRUP"),
                           entries = "EH38E3440167_ENSG00000000419",
@@ -24,28 +25,28 @@ test_that("fetch_data works as intented", {
     ))
 
     #catch error missing table
-    expect_error(fetch_data(CENTREprecompDb,
+    expect_error(fetch_data(centreprecompdb,
         columns = c("pair", "cor_CRUP"),
         entries = "EH38E3440167",
         column_filter = "symbol38"
     ))
 
     #catch error missing columns
-    expect_error(fetch_data(CENTREprecompDb,
+    expect_error(fetch_data(centreprecompdb,
         table = "crup_cor",
         entries = "EH38E3440167",
         column_filter = "symbol38"
     ))
     
     # #catch error when column name doesnt exist
-    expect_error(fetch_data(CENTREprecompDb,
+    expect_error(fetch_data(centreprecompdb,
                             table = "crup_cor",
                             columns = c("pair", "cor_CRUP", "blipblup"),
                             entries = "EH38E3440167",
                             column_filter = "symbol38"
     ))
 
-    expect_error(fetch_data(CENTREprecompDb,
+    expect_error(fetch_data(centreprecompdb,
         table = "wompwomp",
         columns = c("pair", "cor_CRUP"),
         entries = "EH38E3440167",
