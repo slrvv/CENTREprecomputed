229bffe5353778195effeb905dce6f21356cc063
saral98 <sarl98@mi.fu-berlin.de>
2025-08-19 15:14:50 +0200

    change the name of function fetch_data() to fetch_data_precomp()

--- tests/testthat/test_queryEngine.R
+++ tests/testthat/test_queryEngine.R
@@ -1,13 +1,13 @@
 
 
-test_that("fetch_data works as intented", {
+test_that("fetch_data_precomp works as intented", {
     eh <- ExperimentHub::ExperimentHub() 
     centreprecompdb <- eh[["EH9540"]]
     expected <- data.frame(
         pair = c("EH38E3440167_ENSG00000000419"),
         cor_CRUP = c(-0.006860231)
     )
-    fetched <- fetch_data(centreprecompdb,
+    fetched <- fetch_data_precomp(centreprecompdb,
                           table = "crup_cor",
                           columns = c("pair", "cor_CRUP"),
                           entries = "EH38E3440167_ENSG00000000419",
@@ -17,7 +17,7 @@ test_that("fetch_data works as intented", {
     expect_equal(fetched, expected, tolerance = 1e-6)
     
     # catch error missing Db oject
-    expect_error(fetch_data(
+    expect_error(fetch_data_precomp(
         table = "crup_cor",
         columns = c("pair", "cor_CRUP"),
         entries = "EH38E3440167",
@@ -25,28 +25,28 @@ test_that("fetch_data works as intented", {
     ))
 
     #catch error missing table
-    expect_error(fetch_data(centreprecompdb,
+    expect_error(fetch_data_precomp(centreprecompdb,
         columns = c("pair", "cor_CRUP"),
         entries = "EH38E3440167",
         column_filter = "symbol38"
     ))
 
     #catch error missing columns
-    expect_error(fetch_data(centreprecompdb,
+    expect_error(fetch_data_precomp(centreprecompdb,
         table = "crup_cor",
         entries = "EH38E3440167",
         column_filter = "symbol38"
     ))
     
     # #catch error when column name doesnt exist
-    expect_error(fetch_data(centreprecompdb,
+    expect_error(fetch_data_precomp(centreprecompdb,
                             table = "crup_cor",
                             columns = c("pair", "cor_CRUP", "blipblup"),
                             entries = "EH38E3440167",
                             column_filter = "symbol38"
     ))
 
-    expect_error(fetch_data(centreprecompdb,
+    expect_error(fetch_data_precomp(centreprecompdb,
         table = "wompwomp",
         columns = c("pair", "cor_CRUP"),
         entries = "EH38E3440167",
